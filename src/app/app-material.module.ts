7c7f53f66c9aea9cb0afc4af7c13ca1cad384be7
dmunozse <dmunozse.pex@sanitas.es>
2019-03-17 17:18:50 +0100

    Añadido el footer de la página y el layout básico de la aplicación.

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [ MatIconModule, MatToolbarModule ],
-  imports: [ MatIconModule, MatToolbarModule ]
+  exports: [ MatCardModule, MatIconModule, MatSidenavModule, MatToolbarModule ],
+  imports: [ MatCardModule, MatIconModule, MatSidenavModule, MatToolbarModule ]
 })
 export class AppMaterialModule { }
