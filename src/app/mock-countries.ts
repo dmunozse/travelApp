b8087463887726c95067dba9b9ddc5a6ee93be0b
dmunozse <davidmuser91@gmail.com>
2019-04-03 21:27:55 +0200

    Actualizadas las imágenes de los países.

--- src/app/mock-countries.ts
+++ src/app/mock-countries.ts
@@ -5,13 +5,13 @@ export const COUNTRIES: Country[] = [
   { continent: 'NA', id: 2, name: 'Canadá', src: '/assets/countries/canada.jpg', url: 'canada'},
   { continent: 'SA', id: 3, name: 'Brasil', src: '/assets/countries/brazil.jpg', url: 'brazil'},
   { continent: 'SA', id: 4, name: 'Argentina', src: '/assets/countries/argentina.jpg', url: 'argentina'},
-  { continent: 'SA', id: 5, name: 'Chile', src: '', url: 'chile'},
-  { continent: 'AS', id: 6, name: 'China', src: '', url: 'china'},
-  { continent: 'AS', id: 7, name: 'Japón', src: '', url: 'japan'},
+  { continent: 'SA', id: 5, name: 'Chile', src: '/assets/countries/chile.jpg', url: 'chile'},
+  { continent: 'AS', id: 6, name: 'China', src: '/assets/countries/china.jpg', url: 'china'},
+  { continent: 'AS', id: 7, name: 'Japón', src: '/assets/countries/japan.jpg', url: 'japan'},
   { continent: 'EU', id: 8, name: 'España', src: '/assets/countries/spain.jpg', url: 'spain'},
-  { continent: 'EU', id: 9, name: 'Francia', src: '', url: 'france'},
-  { continent: 'EU', id: 10, name: 'Italia', src: '', url: 'italy'},
-  { continent: 'AF', id: 11, name: 'Marruecos', src: '', url: 'morocco'},
-  { continent: 'OC', id: 12, name: 'Australia', src: '', url: 'australia'},
-  { continent: 'OC', id: 13, name: 'Nueva Zelanda', src: '', url: 'new-zealand'}
+  { continent: 'EU', id: 9, name: 'Francia', src: '/assets/countries/france.jpg', url: 'france'},
+  { continent: 'EU', id: 10, name: 'Italia', src: '/assets/countries/italy.jpg', url: 'italy'},
+  { continent: 'AF', id: 11, name: 'Marruecos', src: '/assets/countries/morocco.jpg', url: 'morocco'},
+  { continent: 'OC', id: 12, name: 'Australia', src: '/assets/countries/australia.jpg', url: 'australia'},
+  { continent: 'OC', id: 13, name: 'Nueva Zelanda', src: '/assets/countries/new-zealand.jpg', url: 'new-zealand'}
 ];
