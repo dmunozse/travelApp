4c59c4ddacfaab0f06206373c385c8dc72cf1935
dmunozse <davidmuser91@gmail.com>
2019-04-10 21:01:43 +0200

    Añadidas las miniaturas de las imágenes de los países y contenido en la Home.

--- src/app/mock-countries.ts
+++ src/app/mock-countries.ts
@@ -1,17 +1,17 @@
 import { Country } from './country';
 
 export const COUNTRIES: Country[] = [
-  { continent: 'NA', id: 1, name: 'Estados Unidos', src: '/assets/countries/united-states.jpg', url: 'united-states'},
-  { continent: 'NA', id: 2, name: 'Canadá', src: '/assets/countries/canada.jpg', url: 'canada'},
-  { continent: 'SA', id: 3, name: 'Brasil', src: '/assets/countries/brazil.jpg', url: 'brazil'},
-  { continent: 'SA', id: 4, name: 'Argentina', src: '/assets/countries/argentina.jpg', url: 'argentina'},
-  { continent: 'SA', id: 5, name: 'Chile', src: '/assets/countries/chile.jpg', url: 'chile'},
-  { continent: 'AS', id: 6, name: 'China', src: '/assets/countries/china.jpg', url: 'china'},
-  { continent: 'AS', id: 7, name: 'Japón', src: '/assets/countries/japan.jpg', url: 'japan'},
-  { continent: 'EU', id: 8, name: 'España', src: '/assets/countries/spain.jpg', url: 'spain'},
-  { continent: 'EU', id: 9, name: 'Francia', src: '/assets/countries/france.jpg', url: 'france'},
-  { continent: 'EU', id: 10, name: 'Italia', src: '/assets/countries/italy.jpg', url: 'italy'},
-  { continent: 'AF', id: 11, name: 'Marruecos', src: '/assets/countries/morocco.jpg', url: 'morocco'},
-  { continent: 'OC', id: 12, name: 'Australia', src: '/assets/countries/australia.jpg', url: 'australia'},
-  { continent: 'OC', id: 13, name: 'Nueva Zelanda', src: '/assets/countries/new-zealand.jpg', url: 'new-zealand'}
+  { continent: 'NA', id: 1, name: 'Estados Unidos', src: '/assets/countries/united-states', url: 'united-states'},
+  { continent: 'NA', id: 2, name: 'Canadá', src: '/assets/countries/canada', url: 'canada'},
+  { continent: 'SA', id: 3, name: 'Brasil', src: '/assets/countries/brazil', url: 'brazil'},
+  { continent: 'SA', id: 4, name: 'Argentina', src: '/assets/countries/argentina', url: 'argentina'},
+  { continent: 'SA', id: 5, name: 'Chile', src: '/assets/countries/chile', url: 'chile'},
+  { continent: 'AS', id: 6, name: 'China', src: '/assets/countries/china', url: 'china'},
+  { continent: 'AS', id: 7, name: 'Japón', src: '/assets/countries/japan', url: 'japan'},
+  { continent: 'EU', id: 8, name: 'España', src: '/assets/countries/spain', url: 'spain'},
+  { continent: 'EU', id: 9, name: 'Francia', src: '/assets/countries/france', url: 'france'},
+  { continent: 'EU', id: 10, name: 'Italia', src: '/assets/countries/italy', url: 'italy'},
+  { continent: 'AF', id: 11, name: 'Marruecos', src: '/assets/countries/morocco', url: 'morocco'},
+  { continent: 'OC', id: 12, name: 'Australia', src: '/assets/countries/australia', url: 'australia'},
+  { continent: 'OC', id: 13, name: 'Nueva Zelanda', src: '/assets/countries/new-zealand', url: 'new-zealand'}
 ];
